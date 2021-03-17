4f2053a2eb0afe2cfabf1a5a3c855e575881c604
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-17 16:18:39 -0300

    method mudar imagem

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,48 +1,42 @@
 <template>
   <div id="poke">
-    
-    
-
     <div class="card">
       <div class="card-image">
         <figure>
-          <img
-            :src="pokemon.front"
-            alt="Placeholder image"
-          />
+          <img :src="pokemon.front" alt="Placeholder image" />
         </figure>
       </div>
       <div class="card-content">
-        <div class="media">
-          
-          </div>
-          <div class="media-content">
-            <p class="title is-4">{{ num }} - {{ name | upper }}</p>
-            <p class="subtitle is-6">{{ pokemon.type }}</p>
-          </div>
+        <div class="media"></div>
+        <div class="media-content">
+          <p class="title is-4">{{ num }} - {{ name | upper }}</p>
+          <p class="subtitle is-6">{{ pokemon.type }}</p>
         </div>
+      </div>
 
-        <div class="content">
-        <h1>vasio</h1>
-        </div>
+      <div class="content">
+        <button class="button is-fullwidth" @click="mudarSprite">Mudar Sprite</button>
+      </div>
     </div>
-      
-    </div>
+  </div>
 </template>
 <script>
 import axios from "axios";
 
 export default {
   created: async function () {
-    axios.get(this.url).then((res) => {
+    await axios.get(this.url).then((res) => {
       this.pokemon.type = res.data.types[0].type.name;
       this.pokemon.front = res.data.sprites.front_default;
       this.pokemon.back = res.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
       console.log(this.pokemon);
     });
   },
   data() {
     return {
+      currentImg: "",
+      isFront: true,
       pokemon: {
         type: "",
         front: "",
@@ -61,11 +55,22 @@ export default {
       return newName;
     },
   },
+  methods: {
+    mudarSprite: function () {
+      if (this.isFront) {
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      }else{
+          this.isFront = true
+          this.currentImg = this.pokemon.front
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
-#poke{
-    margin-top:2%;
+#poke {
+  margin-top: 2%;
 }
 </style>
