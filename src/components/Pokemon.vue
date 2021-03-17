cf243d0fb763b1ddb83e8363413b88a8adce3a74
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-17 14:45:51 -0300

    obtendo infos sobre cada pokemon - get

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,24 +1,39 @@
 <template>
   <div>
-    <h1>{{num}} : {{ name | upper }}</h1>
+    <h1>{{ num }} : {{ name | upper }}</h1>
     <small>{{ url }}</small>
-
   </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
+  created: async function () {
+    axios.get(this.url).then((res) => {
+        this.pokemon.type = res.data.types[0].type.name;
+        this.pokemon.front = res.data.sprites.front_default
+        this.pokemon.back = res.data.sprites.back_default
+        console.log(this.pokemon)
+    })
+  },
+  data(){
+      return{
+          pokemon:{
+
+          }
+      }
+  },
   props: {
     name: String,
     url: String,
-    num:Number
+    num: Number,
   },
-  filters:{
-      upper:function(value){
-         
-          let newName = value[0].toUpperCase() + value.slice(1);
-          return newName
-      }
-  }
+  filters: {
+    upper: function (value) {
+      let newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
+    },
+  },
 };
 </script>
 
